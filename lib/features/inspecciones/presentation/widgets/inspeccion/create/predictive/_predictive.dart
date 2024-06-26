7650b0edb39d547207dfeb98dc5882865fb6fb94
samhernandezc24 <samd.hernandezc24@gmail.com>
2024-06-25 22:45:46 -0600

    Widget del predictivo

---
+++ lib/features/inspecciones/presentation/widgets/inspeccion/create/predictive/_predictive.dart
@@ -0,0 +1,15 @@
+import 'package:eos_mobile/shared/shared_libraries.dart';
+
+class _Predictive extends StatelessWidget {
+  const _Predictive({
+    Key? key,
+    this.optionsMaxHeight = 200.0,
+  }) : super(key: key);
+
+  final double optionsMaxHeight;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
